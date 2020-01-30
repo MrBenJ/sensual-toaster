9a0f0a5b986a46831a91bc743227481bd186f207
Ben Junya <bjunya@gmail.com>
2020-01-29 19:19:10 -0800

    Remembering the good ol days

--- static/script.js
+++ static/script.js
@@ -1,9 +1,26 @@
-const boxes = Array.from(document.querySelectorAll('.box'));
-console.log('start your js!');
-const onBoxClick = ({ currentTarget }) => {  
-  currentTarget.classList.toggle('expand');
-};
+var toast = document.getElementById('magic');
+var body = document.getElementById('mood_lighting');
 
-boxes.forEach(box => {
-  box.addEventListener('click', onBoxClick);
-});
+toast.classList.add('sensualize');
+
+var COLORS = [
+  'dodgerblue',
+  'hotpink',
+  'yellow',
+  '#BFFF00',
+  '#cd32cd'
+];
+
+var i = 0;
+body.style = 'background-color: ' + COLORS[i];
+i += 1;
+
+setInterval(function() {
+  var body = document.getElementById('mood_lighting');
+
+  body.style = 'background-color: ' + COLORS[i];
+  i += 1;
+  if (i === COLORS.length) {
+    i = 0;
+  }
+}, 5000);
