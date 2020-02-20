916bc6fbd66ba4d9027249705335d0941e1c90df
Ben Junya <bjunya@gmail.com>
2020-02-19 19:37:24 -0800

    More deterministic behavior and no more flying toast, despite flying toast being extremely sensual

--- static/script.js
+++ static/script.js
@@ -1,28 +1,30 @@
-var toast = document.getElementById('magic');
-var body = document.getElementById('mood_lighting');
-
-setTimeout(function() {
-  toast.classList.add('sensualize');
-}, 0);
-
-var COLORS = [
-  'dodgerblue',
-  'hotpink',
-  'yellow',
-  '#BFFF00',
-  '#cd32cd'
-];
-
-var i = 0;
-body.style = 'background-color: ' + COLORS[i];
-i += 1;
-
-setInterval(function() {
+document.addEventListener('DOMContentLoaded', function() {
+  var toast = document.getElementById('magic');
   var body = document.getElementById('mood_lighting');
 
+  setTimeout(function() {
+    toast.classList.add('sensualize');
+  }, 1000);
+
+  var COLORS = [
+    'dodgerblue',
+    'hotpink',
+    'yellow',
+    '#BFFF00',
+    '#cd32cd'
+  ];
+
+  var i = 0;
   body.style = 'background-color: ' + COLORS[i];
   i += 1;
-  if (i === COLORS.length) {
-    i = 0;
-  }
-}, 5000);
+
+  setInterval(function() {
+    var body = document.getElementById('mood_lighting');
+
+    body.style = 'background-color: ' + COLORS[i];
+    i += 1;
+    if (i === COLORS.length) {
+      i = 0;
+    }
+  }, 5000);
+});
