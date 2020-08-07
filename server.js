fcbc74678be3df5a5a5e465acf20ee8b62f7c9eb
Ben Junya <bjunya@gmail.com>
2020-08-07 09:53:14 -0700

    Add button that starts seduction

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const PORT = 3000;
+const PORT = 3001;
 const app = express();
 app.use('/static', express.static('static'));
 
--- static/script.js
+++ static/script.js
@@ -1,11 +1,5 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var toast = document.getElementById('magic');
+function startBackgroundColors() {
   var body = document.getElementById('mood_lighting');
-
-  setTimeout(function() {
-    toast.classList.add('sensualize');
-  }, 1000);
-
   var COLORS = [
     'dodgerblue',
     'hotpink',
@@ -27,4 +21,27 @@ document.addEventListener('DOMContentLoaded', function() {
       i = 0;
     }
   }, 5000);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  var toast = document.getElementById('magic');
+
+  var startButton = document.getElementById('start');
+
+  startButton.addEventListener('click', function() {
+
+    toast.classList.add('sensualize');
+    startButton.classList.add('hidden');
+    startBackgroundColors();
+
+    var iframe = document.createElement('iframe');
+    iframe.classList.add('music');
+    iframe.width = '560';
+    iframe.height = '315';
+    iframe.src = 'https://www.youtube.com/embed/rjlSiASsUIs?autoplay=1';
+    iframe.frameBorder = '0';
+    iframe.allow = 'accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture';
+    iframe.allowFullscreen = true;
+    document.body.appendChild(iframe);
+  });
 });
